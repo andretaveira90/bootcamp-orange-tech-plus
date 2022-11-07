0e0379fb485a44894319d0be35b4d945edcc708d
taveiradev <andretaveirati@gmail.com>
2022-11-06 22:33:42 -0300

    Aula.

---
+++ 001-Primeiras_paginas_interativas_com_JavaScript/002-Conhecendo_funcoes_JavaScript/002-Praticando_com_funcoes/aula.js
@@ -0,0 +1,15 @@
+function digaMeuNome(name) {
+  console.log('Meu nome é ' + name);
+}
+
+digaMeuNome('André');
+
+function verificarIdade(idade) {
+  if (idade >= 18) {
+    console.log('Maior de idade.');
+  } else {
+    console.log('Menor de idade.');
+  }
+}
+
+verificarIdade(32);
