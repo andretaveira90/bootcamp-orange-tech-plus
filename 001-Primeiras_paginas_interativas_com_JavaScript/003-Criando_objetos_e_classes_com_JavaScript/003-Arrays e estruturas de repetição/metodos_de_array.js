c36248bf825bb4ef76e042d166632212b90dfac1
taveira90 <andretaveirati@gmail.com>
2022-11-28 23:28:46 -0300

    Anotações sobre alguns métodos de array.

---
+++ 001-Primeiras_paginas_interativas_com_JavaScript/003-Criando_objetos_e_classes_com_JavaScript/003-Arrays e estruturas de repetição/metodos_de_array.js
@@ -0,0 +1,23 @@
+// Métodos para lidar com os elementos dos arrays
+
+const alunos = ['André', 'Carlos', 'Adriano', 'Juliano'];
+console.log(alunos);
+
+// Imprimir um elemento
+console.log(alunos[0]);
+
+// Adicionar elementos ao final do array
+alunos.push('Alex') 
+console.log(alunos);
+
+// Adicionar elementos ao início do array
+alunos.unshift('Vagner')
+console.log(alunos);
+
+// Remover elementos do final do array
+alunos.pop()
+console.log(alunos);
+
+// Remover elementos do início do array
+alunos.shift()
+console.log(alunos);
