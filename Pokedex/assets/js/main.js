46bb74c4b12b69aba4d83309dca052b03dc4f6ad
taveira90 <andretaveirati@gmail.com>
2022-12-05 23:57:46 -0300

    Promisse com fetch.

--- Pokedex/assets/js/main.js
+++ Pokedex/assets/js/main.js
@@ -1,1 +1,9 @@
-console.log('Sucesso!');
+
+const offset = 0
+const limit = 10
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+fetch(url) // promisse
+  .then((response) => response.json())
+  .then((jsonBody) => console.log(jsonBody)) // recebe o retorno do then anterior
+  .catch((error) => console.log(error))
